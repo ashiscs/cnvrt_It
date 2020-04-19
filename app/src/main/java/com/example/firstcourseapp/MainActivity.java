8e9dccd85a93fa1757ec2465a8284d39446429b6
ashiscs <ashissingh640@gmail.com>
2020-04-19 21:36:01 +0530

    Use of buttons
    Signed-off-by: ashiscs <ashissingh640@gmail.com>

--- app/src/main/java/com/example/firstcourseapp/MainActivity.java
+++ app/src/main/java/com/example/firstcourseapp/MainActivity.java
@@ -3,8 +3,21 @@ package com.example.firstcourseapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+
+        Log.i("Info","Button Pressed !!");
+    }
+
+    public void clickIt(View view) {
+
+        Log.i("Info","Amazing, you clicked it !!");
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
